6b7b165c223d7267db858e16949294adec73ad25
Carter Peel <carterpeel@gmail.com>
2021-07-21 22:00:50 -0400

    Swapped out if statements
    Swapped out a bunch of ugly if statements for pretty switch cases.

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
-package gosed
+package gosed 
 
 import (
 	"fmt"
 	"golang.org/x/sys/unix"
 	"io"
 	"os"
-	"log"
 )
 
 func ReplaceIn(f *os.File, original, new []byte) error {
@@ -15,7 +14,6 @@ func ReplaceIn(f *os.File, original, new []byte) error {
 	case f == nil:
 		return fmt.Errorf("cannot use nil value as argument for file")
 	}
-
 	fStat, err := f.Stat()
 	if err != nil {
 		return err
@@ -31,7 +29,6 @@ func ReplaceIn(f *os.File, original, new []byte) error {
 		break
 	}
 
-
 	in := make([]byte, 1)
 	readBuffer := make([]byte, 0) // when new > original, we have to read the bytes we will override
 	matchIndex := 0
@@ -39,11 +36,12 @@ func ReplaceIn(f *os.File, original, new []byte) error {
 	readDone := false
 	// If file is not at eof, read from file. else, read from our buffer if it's not empty
 	DoRead := func(b []byte, index int64, appendReadBuffer bool) (int, error) {
-		if readDone {
-			if appendReadBuffer {
+		switch {
+		case readDone:
+			switch {
+			case appendReadBuffer:
 				return 0, nil
-			}
-			if len(readBuffer) == 0 {
+			case len(readBuffer) == 0:
 				return 0, io.EOF
 			}
 			n := 0
@@ -53,19 +51,21 @@ func ReplaceIn(f *os.File, original, new []byte) error {
 			readBuffer = readBuffer[n:]
 			//fmt.Println("DoRead:", len(b), n, string(b[:n]), string(readBuffer))
 			return n, nil
-		} else {
+		default:
 			n, err := f.ReadAt(b, index)
-			if err != nil {
+			switch {
+			case err != nil:
 				readDone = true
-			}
-			if appendReadBuffer {
+			case appendReadBuffer:
 				return n, nil
 			}
 			readBuffer = append(readBuffer, b[:n]...)
-			if n < len(b) {
-				if len(b) > len(readBuffer) {
+			switch {
+			case n < len(b):
+				switch {
+				case len(b) > len(readBuffer):
 					n = len(readBuffer)
-				} else {
+				default:
 					n = len(b)
 				}
 			}
@@ -74,24 +74,28 @@ func ReplaceIn(f *os.File, original, new []byte) error {
 			}
 			readBuffer = readBuffer[n:]
 			//fmt.Println("DoRead:", len(b), n, string(b[:n]), string(readBuffer))
-			if n != 0 {
-				err = nil
+			switch {
+			case n != 0:
+				return n, nil
+			default:
+				return n, err
 			}
-			return n, err
 		}
 	}
 	var ri, wi int64
 	for {
-		_, err := DoRead(in, ri, false)
-		if err != nil {
+		if _, err := DoRead(in, ri, false); err != nil {
 			break
 		}
 		//fmt.Printf("in:%v ri:%v wi:%v matchIndex:%v readBuffer:%v newLen:%v\n", string(in), ri, wi, matchIndex, string(readBuffer), newLen)
 		ri++
-		if in[0] == original[matchIndex] {
+		switch {
+		case in[0] == original[matchIndex]:
 			matchIndex++
-			if matchIndex == len(original) {
-				if len(new) > len(original) {
+			switch {
+			case matchIndex == len(original):
+				switch {
+				case len(new) > len(original):
 					in := make([]byte, len(new)-len(original))
 					n, _ := DoRead(in, ri, true) // only to manage buffer
 					ri += int64(n)
@@ -101,37 +105,33 @@ func ReplaceIn(f *os.File, original, new []byte) error {
 				matchIndex = 0
 				newLen += len(new)
 				wi += int64(len(new))
-				//fmt.Println("matched")
 			}
-		} else if matchIndex != 0 {
+		case matchIndex != 0:
 			//fmt.Println("partial match reset")
 			n, _ := f.WriteAt(original[:matchIndex], wi)
 			wi += int64(n)
 			newLen += n
-			if in[0] == original[0] {
+			switch {
+			case in[0] == original[0]:
 				matchIndex = 1
-			} else {
-				_, err = f.WriteAt(in, wi)
-				if err != nil {
-					log.Printf("Error writing '%s' at offset '%d': %s\n", string(in), wi, err.Error())
+			default:
+				if _, err = f.WriteAt(in, wi); err != nil {
 					// We don't return/continue here because the parent conditional will already continue to the next iteration after updating the high-scope variables
+					log.Printf("Error writing '%s' at offset '%d': %s\n", string(in), wi, err.Error())
 				}
 				wi++
 				newLen++
 				matchIndex = 0
 			}
-		} else {
-			_, err = f.WriteAt(in, wi)
-			if err != nil {
+		default:
+			if _, err = f.WriteAt(in, wi); err != nil {
 				log.Printf("Error writing '%s' at offset '%d': %s\n", string(in), wi, err.Error())
-				// We don't return/continue here because the parent conditional will already continue to the next iteration after updating the high-scope variables
 			}
 			wi++
 			newLen++
 		}
 	}
-	err = f.Truncate(int64(newLen))
-	if err != nil {
+	if err = f.Truncate(int64(newLen)); err != nil {
 		log.Printf("Error truncating file: %s\n", err.Error())
 		return err
 	}
